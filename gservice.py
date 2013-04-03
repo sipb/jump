573ec237024805aa0bd85fa01928591932ffd6f8
Joshua Ma <me@joshma.com>
2013-04-02 21:40:47 -0400

    Basic scaffolding code for gservice

--- gservice.py
+++ gservice.py
@@ -1,3 +1,4 @@
+from flask import redirect, render_template, request
 from service import HomeView, LookupView, ManageView, Service
 
 class GHomeView(HomeView):
@@ -12,6 +13,40 @@ class GLookupView(LookupView):
 
 class GManageView(ManageView):
     template_name = 'g/manage.html'
+
+    def generate_identifier():
+        # TODO: generate unique identifier
+        return ""
+
+    def is_taken(identifier):
+        # TODO: check if identifier is taken
+        return False
+
+    def create_short_link(identifier, url):
+        # TODO: save short link
+
+    def post(self):
+        url = request.form.get('url', None)
+        if not url:
+            error = "Please enter a URL."
+            return render_template(self.template_name, error=error)
+
+        # Ensure URL starts with http:// or https://
+        if not (url.startswith("http://") or url.startswith("https://"):
+            url = 'http://' + url
+
+        identifier = request.form.get('identifier', None)
+        if identifier:
+            # Check if identifier is taken
+            if is_taken(identifier):
+                error = "%s is taken - please try another one." % identifier
+                return render_template(self.template_name, error=error)
+        else:
+            # Generate a unique identifier
+            identifier = generate_identifier()
+
+        create_short_link(identifier, url)
+        return redirect(identifier)
 
 class GService(Service):
     home = GHomeView
